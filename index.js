1f426c5b07d5877e42f8de751477eaa918093133
JubayerRahman <jubayerr398@gmail.com>
2023-11-25 02:38:59 +0600

    added db successfully

--- index.js
+++ index.js
@@ -11,6 +11,53 @@ app.use(cors({
 }))
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w7xbhfw.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const TechEmployeeList = client.db("Hero-J-Tech").collection("employee")
+
+    //all eplooyes
+
+    app.get('/employee', async(req, res)=>{
+        const employeeData = TechEmployeeList.find()
+        const result = await employeeData.toArray()
+        res.send(result)
+    })
+
+    app.post('/employee', async(req, res)=>{
+        const employeeData= req.body
+        const result = await TechEmployeeList.insertOne(employeeData)
+        res.send(result)
+    })
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/',(req, res)=>{
     res.send("i am a server")
 })
