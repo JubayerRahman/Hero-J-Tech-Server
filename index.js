4dfbb8558b5323e1760db718c2906c9d69a0f5a7
JubayerRahman <jubayerr398@gmail.com>
2023-11-26 16:19:30 +0600

    employee verification function are added

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const port = process.env.port || 5000
 
 app.use(cors({
     origin:[
-        'http://localhost:5173'
+        'http://localhost:5173',
+        'https://hero-j-tech.web.app',
+        'https://hero-j-tech.firebaseapp.com'
     ]
 }))
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w7xbhfw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,15 +37,40 @@ async function run() {
     //all eplooyes
 
     app.get('/employee', async(req, res)=>{
-        const employeeData = TechEmployeeList.find()
+      let query = {}
+      if (req.query?.email) {
+        query = {email:req.query.email}
+      }
+        const employeeData = TechEmployeeList.find(query)
         const result = await employeeData.toArray()
         res.send(result)
+    })
+
+    app.get("/employee/:id", async(req, res)=>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const result = await TechEmployeeList.findOne(filter)
+      res.send(result)
     })
 
     app.post('/employee', async(req, res)=>{
         const employeeData= req.body
         const result = await TechEmployeeList.insertOne(employeeData)
         res.send(result)
+    })
+
+    app.put("/employee/:id", async(req, res)=>{
+      const id  = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const option = {upsert:true}
+      const updateData = req.body
+      const employeeData = {
+        $set:{
+          varification: updateData.varification
+        }
+      }
+      const result = await TechEmployeeList.updateOne(filter, employeeData, option)
+      res.send(result)
     })
 
 
